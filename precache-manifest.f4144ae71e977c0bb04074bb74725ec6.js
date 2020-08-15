dfa9d11d79bbf0e9d2b64ac1ef41c6db2534de6b
Rahul Dhanasiri <dhanasirirahul@hotmail.com>
2020-08-15 13:38:06 -0500

    Updates

---
+++ precache-manifest.f4144ae71e977c0bb04074bb74725ec6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed78ab4a2cc3356c3705e6b284b8dd85",
+    "url": "/React-Widgets-App/index.html"
+  },
+  {
+    "revision": "4c91a0ac62f85a35be16",
+    "url": "/React-Widgets-App/static/js/2.598500eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Widgets-App/static/js/2.598500eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aee8f0262c982ffe8ed",
+    "url": "/React-Widgets-App/static/js/main.1c344546.chunk.js"
+  },
+  {
+    "revision": "6b69705df628dbb6dbb2",
+    "url": "/React-Widgets-App/static/js/runtime-main.1c0ebf55.js"
+  }
+]);
